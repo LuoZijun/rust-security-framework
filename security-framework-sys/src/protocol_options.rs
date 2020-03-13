fe35b174f8c87c8adcabacc4de6252201c856a32
luozijun <luozijun.assistant@gmail.com>
2020-03-13 21:07:57 +0800

    update

--- security-framework-sys/src/protocol_options.rs
+++ security-framework-sys/src/protocol_options.rs
@@ -5,75 +5,32 @@ use crate::secure_transport::SSLCiphersuiteGroup;
 
 use libc::c_char;
 
-//  ALPN in SecureTransport 
-// https://forums.developer.apple.com/thread/116193
-// 
-// SecIdentityRef identity = ... load the certificate...;
-// sec_protocol_options_set_local_identity(sec_options, sec_identity_create(identity));
-// sec_protocol_options_add_tls_application_protocol(sec_options, "h2");
-// 
 
-// SEC_OBJECT_DECL!(sec_protocol_options);
 // https://developer.apple.com/documentation/security/sec_protocol_options_t?language=objc
-// typedef NSObject<OS_sec_protocol_options> *sec_protocol_options_t;
 pub enum sec_protocol_options { }
 pub type sec_protocol_options_t = *mut sec_protocol_options;
 
 // #include <dispatch/data.h>
 // https://developer.apple.com/documentation/dispatch/dispatch_data_t#discussion
-// typedef NSObject<OS_dispatch_data> *dispatch_data_t;
 pub enum dispatch_data { }
 pub type dispatch_data_t = *mut dispatch_data;
 
 extern "C" {
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_local_identity(options: sec_protocol_options_t, identity: sec_identity_t);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_add_tls_ciphersuite(options: sec_protocol_options_t, ciphersuite: SSLCipherSuite);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_add_tls_ciphersuite_group(options: sec_protocol_options_t, group: SSLCiphersuiteGroup);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_min_version(options: sec_protocol_options_t, version: SSLProtocol);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_max_version(options: sec_protocol_options_t, version: SSLProtocol);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_add_tls_application_protocol(options: sec_protocol_options_t, application_protocol: *const c_char);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_server_name(options: sec_protocol_options_t, server_name: *const c_char);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_diffie_hellman_parameters(options: sec_protocol_options_t, params: dispatch_data_t);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_add_pre_shared_key(options: sec_protocol_options_t, psk: dispatch_data_t, psk_identity: dispatch_data_t);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_tickets_enabled(options: sec_protocol_options_t, tickets_enabled: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_is_fallback_attempt(options: sec_protocol_options_t, is_fallback_attempt: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_resumption_enabled(options: sec_protocol_options_t, resumption_enabled: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_false_start_enabled(options: sec_protocol_options_t, false_start_enabled: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_ocsp_enabled(options: sec_protocol_options_t, ocsp_enabled: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_sct_enabled(options: sec_protocol_options_t, sct_enabled: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_tls_renegotiation_enabled(options: sec_protocol_options_t, renegotiation_enabled: bool);
-
-    // API_AVAILABLE(macos(10.14), ios(12.0), watchos(5.0), tvos(12.0))
     pub fn sec_protocol_options_set_peer_authentication_required(options: sec_protocol_options_t, peer_authentication_required: bool);
 }
--- security-framework/examples/client.rs
+++ security-framework/examples/client.rs
@@ -1,34 +1,46 @@
 extern crate security_framework;
+
 use security_framework::secure_transport::ClientBuilder;
-use std::io::{Read, Write};
+use security_framework::secure_transport::ClientHandshakeError;
+
+use std::io::{self, Read, Write};
 use std::net::TcpStream;
 
-fn main() {
-    let stream = TcpStream::connect("google.com:443").unwrap();
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let domain = "video.qq.com";
+    let port   = 443u16;
+    
+    let stream = TcpStream::connect(format!("{}:{}", domain, port))?;
+    
     let mut builder = ClientBuilder::new();
     builder.use_sni(true);
 
     #[cfg(feature = "OSX_10_14")]
     builder.alpn_protocols(&["h2", "http/1.1"]);
 
-    let mut stream = builder.handshake("google.com", stream).unwrap();
-    println!(
-        "negotiated chipher: {:?}",
-        stream.context().negotiated_cipher().unwrap()
-    );
-    println!(
-        "negotiated version: {:?}",
-        stream.context().negotiated_protocol_version().unwrap()
-    );
-
+    let mut ssl_stream = builder.handshake(domain, stream)
+        .map_err(|handshake_error| {
+            match handshake_error {
+                ClientHandshakeError::Failure(e) => io::Error::new(io::ErrorKind::Other, e),
+                ClientHandshakeError::Interrupted(_) => {
+                    io::Error::new(io::ErrorKind::WouldBlock, "The handshake was interrupted midway through.")
+                }
+            }
+        })?;
+    
+    let ssl_context = ssl_stream.context();
+    println!("negotiated chipher: {:?}", ssl_context.negotiated_cipher()?);
+    println!("negotiated version: {:?}", ssl_context.negotiated_protocol_version()?);
     #[cfg(feature = "OSX_10_14")]
-    println!("alpn: {:?}", stream.context().alpn_protocols() );
+    println!("negotiated alpns: {:?}", ssl_context.alpn_protocols()?);
 
-    stream.write_all(b"GET / HTTP/1.1\r\n\r\n").unwrap();
-    stream.flush().unwrap();
+    ssl_stream.write_all(b"GET / HTTP/1.1\r\n\r\n")?;
+    ssl_stream.flush()?;
 
     let mut buf = vec![0u8; 1024];
-    let amt = stream.read(&mut buf).unwrap();
+    let amt = ssl_stream.read(&mut buf)?;
     println!("{}", String::from_utf8_lossy(&buf[..amt]));
+
+    Ok(())
 }
